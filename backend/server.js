6045aa89428da71bde18ff571f3c82298460ac4d
Krishna321-cyber <mahatopkrishna439@gmail.com>
2026-06-01 21:32:28 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,4 +29,5 @@ app.use("/api/receipts", receiptRoutes);
 // Forecast Routes
 app.use("/api/forecast", forecastRoutes);
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
